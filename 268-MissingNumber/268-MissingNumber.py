296753d2f656bd33738e9a4b99b241750fece188
Geoffrey Wang <geoffreywang1998@gmail.com>
2025-11-02 20:23:20 -0500

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.py
@@ -0,0 +1,8 @@
+# Last updated: 11/2/2025, 8:23:20 PM
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
