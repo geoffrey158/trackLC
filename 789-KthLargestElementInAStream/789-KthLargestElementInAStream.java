c545376929100480f7b2d25cc157fd18ec99f1c0
Geoffrey Wang <geoffreywang1998@gmail.com>
2025-11-02 20:22:21 -0500

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.java
@@ -0,0 +1,27 @@
+// Last updated: 11/2/2025, 8:22:21 PM
+class KthLargest {
+    private static int k;
+    private PriorityQueue<Integer> heap;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        heap = new PriorityQueue<>();
+        
+        for (int num: nums) {
+            heap.offer(num);
+        }
+        
+        while (heap.size() > k) {
+            heap.poll();
+        }
+    }
+    
+    public int add(int val) {
+        heap.offer(val);
+        if (heap.size() > k) {
+            heap.poll();
+        }
+
+        return heap.peek();
+    }
+}
