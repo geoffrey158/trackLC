be220db7f93cbed34eb1f0f1c2f45dae3ef5e959
Geoffrey Wang <geoffreywang1998@gmail.com>
2025-11-28 23:34:24 -0500

    File updated at 11/28/2025, 11:34:24 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,30 +1,30 @@
-# Last updated: 11/2/2025, 8:23:15 PM
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = [] #result output
-        count = {} #key = number, value = frequnecy of number 
-
-        # Create buckets: an array of empty lists
-        # Index = frequency, Value = list of numbers with that frequency
-        # Edge case:len(nums) + 1 buckets since max frequency = len(nums)
-        freq = [[] for i in range(len(nums)+1)] 
-
-        # Step 1: Count frequencies of each number in nums
-        for n in nums: 
-            count[n] = 1 + count.get(n,0)
-
-        # Step 2: Place numbers into the corresponding frequency bucket
-        # Example: if number 5 appears 3 times → freq[3].append(5)
-        for n,c in count.items(): #n =element, c = frequency 
-            freq[c].append(n)
-        
-        # Step 3: Traverse the buckets backwards (from high freq to low) and collect numbers until we have k of them
-        for i in range(len(freq)-1,-1,-1):
-            for n in freq[i]:
-                res.append(n) 
-                if len(res) == k: # stop once we have k elements in res list
-                    return res
-
-        #Time: O(n), iterates through all elements once in the worst case 
-        #Space: O(n), since we use hashmap and bucket list 
-            
+# Last updated: 11/28/2025, 11:34:24 PM
+1class Solution:
+2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+3        res = [] #result output
+4        count = {} #key = number, value = frequnecy of number 
+5
+6        # Create buckets: an array of empty lists
+7        # Index = frequency, Value = list of numbers with that frequency
+8        # Edge case:len(nums) + 1 buckets since max frequency = len(nums)
+9        freq = [[] for i in range(len(nums)+1)] 
+10
+11        # Step 1: Count frequencies of each number in nums
+12        for n in nums: 
+13            count[n] = 1 + count.get(n,0)
+14
+15        # Step 2: Place numbers into the corresponding frequency bucket
+16        # Example: if number 5 appears 3 times → freq[3].append(5)
+17        for n,c in count.items(): #n =element, c = frequency 
+18            freq[c].append(n)
+19        
+20        # Step 3: Traverse the buckets backwards (from high freq to low) and collect numbers until we have k of them
+21        for i in range(len(freq)-1,-1,-1):
+22            for n in freq[i]:
+23                res.append(n) 
+24                if len(res) == k: # stop once we have k elements in res list
+25                    return res
+26
+27        #Time: O(n), iterates through all elements once in the worst case 
+28        #Space: O(n), since we use hashmap and bucket list 
+29            
