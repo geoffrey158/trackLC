7bcffe9d7a9c209a6f646530ffb5185cddf9b2b0
Geoffrey Wang <geoffreywang1998@gmail.com>
2025-12-07 06:51:13 -0500

    File updated at 12/7/2025, 6:51:13 AM

--- 739-DailyTemperatures/739-DailyTemperatures.py
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -1,19 +1,23 @@
-# Last updated: 11/2/2025, 8:22:23 PM
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        #return list, default to 0 in case there is no warmer days 
-        res = [0] * len(temperatures)
-        stack = []  #stores indices, stack is decreasing in temperatures
-        #i - index, t - temperature
-        for i, t in enumerate(temperatures):
-            # While stack is not empty AND current day's temperature is warmer # than the temperature at the index on top of the stack
-            while stack and t > temperatures[stack[-1]]:
-                index = stack.pop()   #Index of the previous cooler day
-                res[index] = i - index  #Days waited until warmer temperature
-
-            #Push current day's index onto the stack
-            stack.append(i)
-
-        return res
-    #Time Complexity O(n), Each index is pushed & popped at most once. 
-    #Space Complexity O(n), For the stack and output array.
+# Last updated: 12/7/2025, 6:51:13 AM
+1class Solution:
+2    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+3        #return list, Default is 0 because if there's no future warmer day, answer stays 0.
+4        res = [0] * len(temperatures) 
+5        stack = []  #stores indices, monotonic *decreasing* used to store the INDICES of tempeatures
+6
+7        #loop throught each index and temperature 
+8        for idx, temp in enumerate(temperatures):
+9
+10            # While stack is not empty AND current day's temperature is warmer # than the temperature at the index on top of the stack
+11            # That means we found a warmer future day for stack[-1]
+12            while stack and temp > temperatures[stack[-1]]:
+13                index = stack.pop()   #Index of the previous cooler day
+14                res[index] = idx - index  #Days waited until warmer temperature
+15
+16            # Push current day's index onto the stack
+17            # That means we found a warmer future day for stack[-1].
+18            stack.append(idx)
+19
+20        return res
+21    #Time Complexity O(n), Each index is pushed & popped at most once. 
+22    #Space Complexity O(n), For the stack and output array.
