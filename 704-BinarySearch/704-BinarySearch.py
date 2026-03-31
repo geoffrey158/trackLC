8e9dd0d51c18fe5fc11ebb36c464483ae11f4cbc
Geoffrey Wang <geoffreywang1998@gmail.com>
2026-03-31 19:09:40 -0400

    File updated at 3/31/2026, 7:09:39 PM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,25 +1,17 @@
-# Last updated: 1/20/2026, 6:18:37 AM
+# Last updated: 3/31/2026, 7:09:39 PM
 1class Solution:
 2    def search(self, nums: List[int], target: int) -> int:
-3        
-4        #binary search you start with a left and right poitner and then converage into the middle and search like that 
-5        #keep on looping until we find our answer
-6
-7        left = 0 
-8        right = len(nums)-1 
+3        left = 0
+4        right = len(nums)-1
+5
+6        while left <= right:
+7
+8            mid = left + (right-left)//2 
 9
-10        #<= bc the list could be empty so we use for that
-11        while left <= right:
-12            mid = left + (right-left)//2 
-13
-14            if nums[mid] == target:
-15                return mid # return index 
-16            elif nums[mid] > target:
-17                right = mid-1 
-18            else:
-19                left = mid+1
-20            
-21        return -1 #if the target is not found
-22
-23
-24            
+10            if nums[mid] == target:
+11                return mid 
+12            elif nums[mid] > target:
+13                right = mid - 1
+14            else:
+15                left = mid + 1 
+16        return -1 
